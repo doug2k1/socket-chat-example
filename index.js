8ebec1053f20636387b6d8a45a67edb0155c6e7f
Douglas Matoso <doug2k1@gmail.com>
2016-01-29 23:32:27 -0200

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var PORT = 3000;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('user connected');
+
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(PORT, function () {
+    console.log('HTTP server started on port ' + PORT);
+})
